f878013ff806d738d365e964aa4f223e5a44655d
imran khan <imrankhan.ca2020@gmail.com>
2025-10-04 11:54:30 +0530

    secound commit

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-
-// vite.config.js
-export default {
-  build: {
-    target: 'es2018'  // या 'es2020' आदि, जरूरत के हिसाब से
-  }
-}
-
